10f5d26928bb5ca48d74ced6c881727bb3fa25d2
Kegen Guyll <guyllkegen@gmail.com>
2021-09-05 18:15:34 -0500

    created middleware

---
+++ src/middleware/async.middleware.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+
+type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<any>;
+
+export default (handler: AsyncRequestHandler): RequestHandler => {
+  return (req, res, next) => {
+    return handler(req, res, next).catch(next);
+  };
+};
