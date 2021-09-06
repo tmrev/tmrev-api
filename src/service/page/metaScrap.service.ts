e54b58d3112542f00e4085091290b51c8a3263fb
Kegen Guyll <guyllkegen@gmail.com>
2021-09-05 19:36:01 -0500

    replaced puppeteer with request-promise

--- src/service/page/metaScrap.service.ts
+++ src/service/page/metaScrap.service.ts
@@ -1,40 +1,30 @@
-import cheerio from 'cheerio';
-import puppeteer from 'puppeteer';
-import startBrowser from '../../../utils/browser';
+import cheerio, { CheerioAPI } from 'cheerio';
+import request from 'request-promise';
 
 export const metaScrapService = async (url: string) => {
   try {
-    const scraperObject = {
-      url,
-      async scraper(browser: puppeteer.Browser) {
-        let page = await browser.newPage();
-        console.log(`Navigating to ${this.url}...`);
-        await page.goto(this.url);
-
-        const content = await page.content();
-
-        const $ = cheerio.load(content);
-
-        const metaDetails = {
-          type: $('meta[property="og:type"]').attr('content') || null,
-          title: $('meta[property="og:title"]').attr('content') || null,
-          url: $('meta[property="og:url"]').attr('content') || null,
-          siteName: $('meta[property="og:site_name"]').attr('content') || null,
-          description:
-            $('meta[property="og:description"]').attr('content') ||
-            $('meta[name="description"]').attr('content') ||
-            null,
-          image: $('meta[property="og:image"]').attr('content') || null,
-        };
-
-        page.close().then(() => console.log('Closing the browser...'));
-        return metaDetails;
+    const options = {
+      uri: url,
+      transform: (body: string) => {
+        return cheerio.load(body);
       },
     };
 
-    const browserInstance = await startBrowser();
+    const meta = request(options).then(($: CheerioAPI) => {
+      const metaDetails = {
+        type: $('meta[property="og:type"]').attr('content') || null,
+        title: $('meta[property="og:title"]').attr('content') || null,
+        url: $('meta[property="og:url"]').attr('content') || null,
+        siteName: $('meta[property="og:site_name"]').attr('content') || null,
+        description:
+          $('meta[property="og:description"]').attr('content') ||
+          $('meta[name="description"]').attr('content') ||
+          null,
+        image: $('meta[property="og:image"]').attr('content') || null,
+      };
 
-    const meta = scraperObject.scraper(browserInstance);
+      return metaDetails;
+    });
 
     return meta;
   } catch (err) {
