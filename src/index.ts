079bf4783cae1f82b61325e3d26761c47cc93c5d
Kegen Guyll <guyllkegen@gmail.com>
2021-11-29 00:12:48 -0600

    added firebase and mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,43 @@
 import app from './app';
 import redis from 'redis';
-import cron from 'cron';
-import request from 'request-promise';
+import { initializeApp } from 'firebase-admin/app';
+import { MongoClient } from 'mongodb';
+
 require('dotenv').config();
 
 const PORT_REDIS = process.env.PORT_REDIS || 6379;
 const PORT = process.env.PORT || 8080;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const host = process.env.DB_HOST;
+const projectId = process.env.PROJECT_ID;
 
 // export const redisClient = redis.createClient({
 //   host: 'redis',
 //   port: Number(PORT_REDIS),
 // });
-
 export const redisClient = redis.createClient(Number(PORT_REDIS));
+export const firebaseApp = initializeApp({
+  projectId,
+});
+
+const uri = `mongodb+srv://${user}:${password}@${host}`;
+
+export const client = new MongoClient(uri);
+
+const retryConnection = () => {
+  client
+    .connect()
+    .then(() => {
+      console.log('mongodb is connected');
+    })
+    .catch((e) => {
+      console.error(e);
+      retryConnection();
+    });
+};
+
+retryConnection();
 
 export const set = (key: string, value: any) => {
   console.log('set');
@@ -23,16 +48,6 @@ export const set = (key: string, value: any) => {
 redisClient.on('connect', () => console.log('redis is connected'));
 
 redisClient.on('error', (err) => console.log(`redis angry ${err}`));
-
-const job = new cron.CronJob(
-  '5 4 * * sun',
-  () => request({ url: 'http://localhost:8080/health' }),
-  null,
-  true,
-  'America/Los_Angeles'
-);
-
-job.start();
 
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
