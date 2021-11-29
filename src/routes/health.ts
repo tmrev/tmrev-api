d90302b3689f0f5d67563350696ee120b4c0db8c
Kegen Guyll <guyllkegen@gmail.com>
2021-11-29 00:07:46 -0600

    created flush route

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { flushCacheController } from '../controllers/health/flushCache.Controller';
 import { generalHealthController } from '../controllers/health/generalHealthController';
 import asyncMiddleware from '../middleware/async.middleware';
 
 const router: Router = Router();
 
 router.get('/', asyncMiddleware(generalHealthController));
+
+router.get('/flush', asyncMiddleware(flushCacheController));
 
 export const healthRouter: Router = router;
