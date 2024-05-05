6386c8e97e5358701cb9888fac857fa6613c0c6c
Kegen Guyll <guyllkegen@gmail.com>
2024-05-05 18:48:38 -0500

    improved createUser service by specifying what should be saved to the DB

--- src/service/users/createUser.service.ts
+++ src/service/users/createUser.service.ts
@@ -3,13 +3,45 @@ import { getAuth } from "firebase-admin/auth";
 import { client } from "../..";
 import { tmrev } from "../../models/mongodb";
 
+type User = {
+  bio: string;
+  createdAt: Date;
+  email: string;
+  firstName: string;
+  followers: string[];
+  following: string[];
+  lastName: string;
+  link: string | null;
+  location: string;
+  photoUrl: string | null;
+  public: boolean;
+  updatedAt: Date;
+  uuid: string;
+};
+
 const createUserService = async (authToken: string, body: any) => {
   try {
     await getAuth().verifyIdToken(authToken);
     const db = client.db(tmrev.db).collection(tmrev.collection.users);
     const feedDB = client.db(tmrev.db).collection(tmrev.collection.feed);
 
-    const { insertedId } = await db.insertOne(body);
+    const user: User = {
+      bio: body.bio,
+      createdAt: new Date(),
+      email: body.email.toLowerCase(),
+      firstName: body.firstName,
+      followers: [],
+      following: [],
+      lastName: body.lastName,
+      link: body.link,
+      location: body.location,
+      photoUrl: body.photoUrl,
+      public: body.public,
+      updatedAt: new Date(),
+      uuid: body.uuid,
+    };
+
+    const { insertedId } = await db.insertOne(user);
 
     const feed = {
       reviews: [],
@@ -24,6 +56,7 @@ const createUserService = async (authToken: string, body: any) => {
 
     return "success";
   } catch (error) {
+    console.error(error);
     return {
       success: false,
       error,
