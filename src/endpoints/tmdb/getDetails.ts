7080f9694f01ac832107dba02fbba46e4d45194f
Kegen Guyll <guyllkegen@gmail.com>
2024-07-30 17:48:37 -0500

    chore: Fix TMDB API URL in getDetails.ts
    The code changes fix the TMDB API URL in the getDetails.ts file by removing the extra slash before "movie". This ensures that the correct URL is used to fetch movie details from the TMDB API.

--- src/endpoints/tmdb/getDetails.ts
+++ src/endpoints/tmdb/getDetails.ts
@@ -4,16 +4,14 @@ import { Movie } from "../../models/tmdb";
 
 const getDetails = async (movieId: number, append = true) => {
   try {
-    const url = `${TMDB_BASE_URL}/movie/${movieId}?api_key=${TMDB_API_KEY}`;
+    const url = `${TMDB_BASE_URL}movie/${movieId}?api_key=${TMDB_API_KEY}`;
     const additional = `&append_to_response=credits,release_dates
 `;
     const res: AxiosResponse<Movie> = await axios(
       `${url}${append ? additional : ""}`
     );
 
-    const data = await res.data;
-
-    return data;
+    return res.data;
   } catch (error) {
     return null;
   }
