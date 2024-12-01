ec26789718106259ee426996fdfedbcbe25b5bba
Kegen Guyll <guyllkegen@gmail.com>
2024-12-01 16:10:57 -0600

    fix getWatchProviders to correct URL formatting and improve error logging

--- src/endpoints/tmdb/getWatchProviders.ts
+++ src/endpoints/tmdb/getWatchProviders.ts
@@ -4,14 +4,15 @@ import { WatchProviderResponse } from "../../models/tmdb";
 
 const getWatchProviders = async (movieId: number) => {
   try {
-    const url = `${TMDB_BASE_URL}/movie/${movieId}/watch/providers?api_key=${TMDB_API_KEY}`;
+    const url = `${TMDB_BASE_URL}movie/${movieId}/watch/providers?api_key=${TMDB_API_KEY}`;
 
     const res: AxiosResponse<WatchProviderResponse> = await axios(url);
 
-    const data = await res.data;
+    const { data } = res;
 
     return data;
   } catch (error) {
+    console.error(error);
     return null;
   }
 };
